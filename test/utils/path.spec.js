0a0e2818bc0b22ad5f52d70a8a37a3b3112759ba
kround <yuan40kang@126.com>
2018-06-27 18:59:38 +0800

    删除未完成的测试

--- test/utils/path.spec.js
+++ test/utils/path.spec.js
@@ -1,13 +1,2 @@
 const expect = require('expect')
 const CreatePath = require('../../libs/utils/path.tool');
-
-describe('Test path tool', () => {
-    it('ReadFile FileStatus README.md true', () => {
-        expect.assertions(1);
-        return FileTool.FileStatus(`./README.md`).then(data => expect(data).toBeTruthy());
-    });
-    it('WriteFile test.json', () => {
-        expect.assertions(1);
-        return FileTool.WriteFile(`./test.json`, 1).then(data => expect(data).toBeTruthy());
-    });
-})
